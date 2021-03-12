2266ce3ef379bda36a13af091e8f2e1ddb0c87aa
Anirudh Kumar Kushwaha <iamanirudh.ak@gmail.com>
2021-03-12 22:19:02 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 sudo chmod +x mac_changer.py
 sudo chmod +x network_scanner.py
+sudo chmod +x arp_spoofer.py
+sudo chmod +x dns_spoof.py
 
 sudo cp mac_changer.py /bin/mac_changer
 sudo cp network_scanner.py /bin/network_scanner
+sudo cp arp_spoofer.py /bin/arp_spoofer
+sudo cp dns_spoof.py /bin/dns_spoof
