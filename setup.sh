67c7c47ac305fb09ddbd6d9c24447ef2dc2b0954
Anirudh Kumar Kushwaha <49266081+AnirudhKK@users.noreply.github.com>
2021-03-13 01:26:45 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
+sudo apt-get install -y python3
+sudo apt install -y python3-pip
+apt-get install -y build-essential python-dev libnetfilter-queue-dev
+
+pip3 install -U git+https://github.com/kti/python-netfilterqueue
+pip3 install  scapy
+pip3 install  scapy_http
+
 sudo chmod +x mac_changer.py
 sudo chmod +x network_scanner.py
 sudo chmod +x arp_spoofer.py
