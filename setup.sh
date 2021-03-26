38b5bd0c2838be034933edc89427232140bbf1ae
Anirudh Kumar Kushwaha <iamanirudh.ak@gmail.com>
2021-03-26 09:54:01 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,9 +7,7 @@ pip3 install  scapy_http
 sudo chmod +x mac_changer.py
 sudo chmod +x network_scanner.py
 sudo chmod +x arp_spoofer.py
-sudo chmod +x dns_spoof.py
 
 sudo cp mac_changer.py /bin/mac_changer
 sudo cp network_scanner.py /bin/network_scanner
 sudo cp arp_spoofer.py /bin/arp_spoofer
-sudo cp dns_spoof.py /bin/dns_spoof
