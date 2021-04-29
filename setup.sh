d700746f6f6aa62cc549a30d7529d98160710453
Anirudh Kumar Kushwaha <iamanirudh.ak@gmail.com>
2021-04-29 23:17:25 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,9 +10,11 @@ sudo chmod +x network_scanner.py
 sudo chmod +x arp_spoofer.py
 sudo chmod +x crawler.py
 sudo chmod +x spider.py
+sudo chmod +x extract_wifi_pass.py
 
 sudo cp mac_changer.py /bin/mac_changer
 sudo cp network_scanner.py /bin/network_scanner
 sudo cp arp_spoofer.py /bin/arp_spoofer
 sudo cp crawler.py /bin/crawler
 sudo cp spider.py /bin/spider
+sudo cp extract_wifi_pass.py /bin/extract_wifi_pass
