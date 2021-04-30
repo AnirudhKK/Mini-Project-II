fd005b7308e70925f6f1f75c8aed137d8628c43f
Anirudh Kumar Kushwaha <iamanirudh.ak@gmail.com>
2021-04-30 10:09:22 +0530

    UPDATE

--- setup.sh
+++ setup.sh
@@ -1,9 +1,19 @@
-sudo apt-get install -y python3
-sudo apt install -y python3-pip
+printf "______________.___._______________________________            _____________________________  
+\_   ___ \__  |   |\______   \_   _____/\______   \          /   _____/\_   _____/\_   ___ \ 
+/    \  \//   |   | |    |  _/|    __)_  |       _/  ______  \_____  \  |    __)_ /    \  \/ 
+\     \___\____   | |    |   \|        \ |    |   \ /_____/  /        \ |        \\\\     \____
+ \______  / ______| |______  /_______  / |____|_  /         /_______  //_______  / \______  /
+        \/\/               \/        \/         \/                  \/         \/         \/ "
+echo "\nInstalling. Have patience\n"
+sudo apt-get install -y python3 > /dev/null 2>&1
+sudo apt install -y python3-pip > /dev/null 2>&1
+echo "\nInstalling.. Have patience\n"
 
-pip3 install scapy
-pip3 install scapy_http
-pip3 install requests
+pip3 install scapy > /dev/null 2>&1
+echo "\nInstalling... Have patience\n"
+pip3 install scapy_http > /dev/null 2>&1
+echo "\nInstalling.... Have patience\n"
+pip3 install requests > /dev/null 2>&1
 
 sudo chmod +x mac_changer.py
 sudo chmod +x network_scanner.py
@@ -19,9 +29,9 @@ sudo cp crawler.py /bin/crawler
 sudo cp spider.py /bin/spider
 sudo cp extract_wifi_pass.py /bin/extract_wifi_pass
 
-printf "Tools have been installed successfully. They can be accessed by following commands :- "
-printf "1. MAC Changer              -  mac_changer [arguments]"
-printf "2. Network Scanner          -  network_scanner [arguments]"
-printf "3. Crawler                  -  crawler [arguments]"
-printf "4. Spider                   -  spider [arguments]"
-printf "5. Wifi-Pass Extractor      -  extract_wifi_pass"
+printf "\nTools have been installed successfully. They can be accessed by following commands :- "
+printf "\n1. MAC Changer              -  mac_changer [arguments]"
+printf "\n2. Network Scanner          -  network_scanner [arguments]"
+printf "\n3. Crawler                  -  crawler [arguments]"
+printf "\n4. Spider                   -  spider [arguments]"
+printf "\n5. Wifi-Pass Extractor      -  extract_wifi_pass"
